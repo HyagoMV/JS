af279081fadaa1081a642fc72cf8299f23dd8c93
Hyago <hyagomv@gmail.com>
2021-01-25 16:34:15 -0300

    Exemplo 7

---
+++ exemplo6.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch'; // modulo que traz window.fetch para o Node.js
+
+const log = console.log;
+
+log("Sync A")
+
+// Timeout
+fetch('https://my-json-serverNO_EXIST.typicode.com/typicode/demo/posts') // throw TIMEDOUT
+  .then(res => res.json())
+  .then(res => log('THEN 1', res))
+  .catch(res => log('CATCH fetch()', res.code));
+
+log("Sync B")
+
+
+
